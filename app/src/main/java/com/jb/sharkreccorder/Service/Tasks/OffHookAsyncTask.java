40449c27f062802016ff69cd088a8c3d73c2f60b
zerocool <bertieaux.jo@gmail.com>
2022-01-16 17:54:32 +0100

    update Logger

--- app/src/main/java/com/jb/sharkreccorder/Service/Tasks/OffHookAsyncTask.java
+++ app/src/main/java/com/jb/sharkreccorder/Service/Tasks/OffHookAsyncTask.java
@@ -5,13 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.jb.sharkreccorder.Model.RecorderConfiguration;
+import com.jb.sharkreccorder.Utils.Constants;
 import com.jb.sharkreccorder.Utils.Logger.Logger;
 import com.jb.sharkreccorder.Utils.Logger.LoggerLevel;
 import com.jb.sharkreccorder.Utils.Observer.IObserver;
 
 public class OffHookAsyncTask extends RadioAsyncTask {
-
-    private final String TAG = "OffHookAsyncTask";
 
     public OffHookAsyncTask(BroadcastReceiver.PendingResult pendingResult, Intent intent, RecorderConfiguration recorder, Context context, IObserver o) {
         super(pendingResult, intent, recorder, context, o);
@@ -19,7 +18,7 @@ public class OffHookAsyncTask extends RadioAsyncTask {
 
     @Override
     protected String doInBackground(String... strings) {
-        Logger.Logging(LoggerLevel.INFOS, TAG, "RADIO_STATE - OFF HOOK : (CALL ANSWERED)");
+        Logger.Logging(LoggerLevel.INFOS, Constants.OFF_HOOK_ASYNC, "RADIO_STATE - OFF HOOK : (CALL ANSWERED)");
         return "CALL ANSWERED";
     }
 
